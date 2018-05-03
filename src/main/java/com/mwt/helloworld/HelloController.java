b9dff086709f13aa1fa7c765c6c8edb50988bd16
Radu Obreja <r.obreja@moodmedia.com>
2018-05-03 12:31:53 +0300

    new change

--- src/main/java/com/mwt/helloworld/HelloController.java
+++ src/main/java/com/mwt/helloworld/HelloController.java
@@ -1,5 +1,7 @@
 package com.mwt.helloworld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
@@ -9,11 +11,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
+	private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @Autowired
     JdbcTemplate jdbcTemplate;
 	
 	@GetMapping("/")
 	String home(ModelMap modal) {
+		log.info("Initializing database");
+
+		jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE customers(id_customer VARCHAR(64), customer_label VARCHAR(255))");
+        jdbcTemplate.execute("INSERT INTO customers(id_customer, customer_label) VALUES ('radu', 'Radu')");
+        jdbcTemplate.execute("INSERT INTO customers(id_customer, customer_label) VALUES ('simo', 'Simo')");
+
 		modal.put("customers", jdbcTemplate.query("SELECT * FROM customers", new MapMapper()));
 		return "index";
 	}
