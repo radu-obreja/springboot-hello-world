df710f844fef3e95ff329f0edc2351be5056aeeb
Radu Obreja <r.obreja@moodmedia.com>
2018-05-03 14:48:10 +0300

    2db

--- src/main/java/com/mwt/helloworld/HelloController.java
+++ src/main/java/com/mwt/helloworld/HelloController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
